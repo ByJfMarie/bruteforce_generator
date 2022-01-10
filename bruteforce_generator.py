377a81147219417e84929a0e8fb8bcc613403e8a
samghami <samghami@gmail.com>
2022-01-10 17:22:28 +0100

    test

--- bruteforce_generator.py
+++ bruteforce_generator.py
@@ -5,23 +5,19 @@ os.system("echo Hello from the other side!")
 
 
 def main(argv):
-   inputfile = ''
-   outputfile = ''
+   wordlistFile = ''
    try:
-      opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+      opts, args = getopt.getopt(argv,"hw",["wordlist="])
    except getopt.GetoptError:
-      print('bruteforce_generator.py -i <inputfile> -o <outputfile>')
+      print('bruteforce_generator.py -w <wordlist>')
       sys.exit(2)
    for opt, arg in opts:
       if opt == '-h':
-         print('bruteforce_generator.py -i <inputfile> -o <outputfile>')
+         print('bruteforce_generator.py -w <wordlist>')
          sys.exit()
-      elif opt in ("-i", "--ifile"):
-         inputfile = arg
-      elif opt in ("-o", "--ofile"):
-         outputfile = arg
-   print ('Input file is "', inputfile)
-   print ('Output file is "', outputfile)
+      elif opt in ("-w", "--wordlist"):
+         wordlistFile = arg
+   print ('Wordlist file is "', wordlistFile)
 
 if __name__ == "__main__":
    main(sys.argv[1:])
