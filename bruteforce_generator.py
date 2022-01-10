42844784daf14add372cbbc1e1e5bd145afb0c2c
samghami <samghami@gmail.com>
2022-01-10 17:11:20 +0100

    os

--- bruteforce_generator.py
+++ bruteforce_generator.py
@@ -1,6 +1,3 @@
-import subprocess
-process = subprocess.Popen(['echo', 'More output'],
-                     stdout=subprocess.PIPE, 
-                     stderr=subprocess.PIPE)
-stdout, stderr = process.communicate()
-stdout, stderr
+import os
+
+os.system("echo Hello from the other side!")
