a017b7f994f8d7b3e4a4037324a52d7b5cb0da79
samghami <samghami@gmail.com>
2022-01-10 17:33:53 +0100

    test

--- bruteforce_generator.py
+++ bruteforce_generator.py
@@ -1,23 +1,4 @@
 #!/usr/bin/python
 import os, sys, getopt
 
-os.system("echo Hello from the other side!")
-
-
-def main(argv):
-   wordlistFile = ''
-   try:
-      opts, args = getopt.getopt(argv,"h:w",["wordlist="])
-   except getopt.GetoptError:
-      print('bruteforce_generator.py -w <wordlist>')
-      sys.exit(2)
-   for opt, arg in opts:
-      if opt == '-h':
-         print('bruteforce_generator.py -w <wordlist>')
-         sys.exit()
-      elif opt in ("-w", "--wordlist"):
-         wordlistFile = arg
-   print (wordlistFile)
-
-if __name__ == "__main__":
-   main(sys.argv[1:])
+print("Wordlist = ", sys.argv[1])
