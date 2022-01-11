3457797b0a2b59d209f5cf545149cf10e956a174
samghami <samghami@gmail.com>
2022-01-11 09:14:15 +0100

    print correction

--- bruteforce_generator.py
+++ bruteforce_generator.py
@@ -5,9 +5,9 @@ try:
     wordlistList = sys.argv[1]
     wordlist = open(wordlistList, "r")
 except:
-    print "Usage: bruteforce_generator.py wordlist.txt"
+    print ("Usage: bruteforce_generator.py wordlist.txt")
     sys.exit(2)
 
 for line in wordlist:
     line = line.strip()
-    print line
+    print (line)
